4c84d62002bb738dda82d51c54b15574e06b6e34
Valher7 <Valentin.herman.pro@gmail.com>
2025-03-28 20:31:58 +0100

    Add script.js

--- src/script.js
+++ src/script.js
@@ -1,1 +1,42 @@
+import { SELECT } from "./components/constants.js";
+import { fetchCitySuggestions } from "./components/suggestion.js";
+import { getCoordonates, fetchApiWeahter } from "./components/api.js";
 
+//Handle of input suggestions
+const INPUT = document.getElementById("cityInput");
+const BUTTON = document.getElementById("searchCityButton");
+
+INPUT.addEventListener("keyup", () => {
+    fetchCitySuggestions(INPUT.value);
+    BUTTON.style.visibility = "hidden";
+});
+
+SELECT.addEventListener("click", () => {
+    if(SELECT.value === "No city found...") {
+        SELECT.style.display = "none";        
+        INPUT.value = "";
+        BUTTON.style.visibility = "hidden";
+    } else {
+    INPUT.value = SELECT.value;
+    SELECT.style.display = "none"; 
+    BUTTON.style.visibility = "visible";
+    }
+});
+
+//Handle of search city button
+
+BUTTON.addEventListener("click", async() => {
+    let results = INPUT.value.match(/[^(),]+/g).map(e => e.trim());
+    
+    let coordinates;
+    if(results.length === 2) {
+        coordinates = await getCoordonates(results[0], results[1]); 
+    }
+    if(results.length === 3) {
+        coordinates = await getCoordonates(results[0], results[1], results[2]);
+    }  
+    let lat = coordinates.lat;
+    let lon = coordinates.lon;
+    let weather = await fetchApiWeahter(lat, lon);
+    console.log(weather);
+})
